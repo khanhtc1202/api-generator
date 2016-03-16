e2ffe46f501ce273270b2264ed5fda0dae41420c
Trần Công Khanh <khanhtc@ows.edu.vn>
2016-03-16 12:13:45 +0700

    fix error gen template

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var MongoClient = require('mongodb').MongoClient;
 var gulp = require('gulp'),
 	template = require('gulp-template'),
     gulp_data = require('gulp-data'),
-    rename = require('gulp-rename'),
-	argv = require('yargs').argv;
+    rename = require('gulp-rename');
+// var argv = require('yargs').argv;
 
 
 
-gulp.task('db', function () {
+gulp.task('gen', function () {
     MongoClient.connect(config.db, function(err, db) {
         db.listCollections({}).toArray().then(function(items) {
 
@@ -57,20 +57,20 @@ function genModels (name, data){
     .pipe(gulp.dest('./app/models/'));
 }
 
-gulp.task('genCtrl', function () {
-    var name = argv.apiname;  
-    gulp.src('./template/ctrlTemplate.js')
-    .pipe(template({name: name}))
-    .pipe(rename(name+'.js'))
-    .pipe(gulp.dest('./app/controllers/'));
-});
+// gulp.task('genCtrl', function () {
+//     var name = argv.apiname;  
+//     gulp.src('./template/ctrlTemplate.js')
+//     .pipe(template({name: name}))
+//     .pipe(rename(name+'.js'))
+//     .pipe(gulp.dest('./app/controllers/'));
+// });
 
-gulp.task('genModels', function () {
-    var name = argv.apiname;  
-    gulp.src('./template/modelsTemplate.js')
-    .pipe(template({name: name}))
-    .pipe(rename(name+'.js'))
-    .pipe(gulp.dest('./app/models/'));
-});
+// gulp.task('genModels', function () {
+//     var name = argv.apiname;  
+//     gulp.src('./template/modelsTemplate.js')
+//     .pipe(template({name: name}))
+//     .pipe(rename(name+'.js'))
+//     .pipe(gulp.dest('./app/models/'));
+// });
 
-gulp.task('gen', ['genCtrl', 'genModels']);
+// gulp.task('gen', ['genCtrl', 'genModels']);
